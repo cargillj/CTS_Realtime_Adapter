4de9786881ede2ded53f280a1b47c8e46b066f7b
John Cargill <cargillj@onid.orst.edu>
2014-04-19 13:33:50 -0700

    created id_list

--- parser.py
+++ parser.py
@@ -1,6 +1,13 @@
 import urllib2
+import json
 
-response = urllib2.urlopen('http://www.corvallis-bus.appspot.com/arrivals?stops=13713')
-html = response.read()
+response = urllib2.urlopen('http://www.corvallis-bus.appspot.com/stops')
+data = response.read()
 
-print html
+id_list = []									#list of bus ids
+
+for i in range(0, len(data)):
+	if data[i:(i+5)] == '"ID":':				#parse for idf info
+		id_list.append(data[(i+5):(i+10)])		
+
+print id_list[0]

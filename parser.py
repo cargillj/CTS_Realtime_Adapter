d8b020fde86568b5bcda38f6f4c1198df9cbc88d
Matt Kirsch <matthewkirsch1@gmail.com>
2014-04-19 14:31:45 -0700

    updated parser logic with properly staged parser

--- parser.py
+++ parser.py
@@ -1,13 +1,31 @@
 import urllib2
 import json
 
+#this calls the stop IDs for processing and saving into a list
 response = urllib2.urlopen('http://www.corvallis-bus.appspot.com/stops')
 data = response.read()
 
-id_list = []							#list of bus ids
+#list of bus ids
+id_list = []							
 
+#parse for id info
 for i in range(0, len(data)):
-	if data[i:(i+5)] == '"ID":':				#parse for idf info
+	if data[i:(i+5)] == '"ID":':				
 		id_list.append(data[(i+5):(i+10)])		
 
-print id_list[0]
+#an array for storing the times of each route 
+timesArray = []									
+
+#text file containing the information in gtfs format 
+gtfsUpdateFile = open("gtfsUpdateFile.txt", "w")
+
+for iterator in range(0, 5):
+	response = urllib2.urlopen('http://www.corvallis-bus.appspot.com/arrivals?stops=' + id_list[iterator])
+	apiresponse = response.read()	
+	timesArray.append(apiresponse)
+
+for i in range(0, len(timesArray)): 
+	 if timesArray[i:(i+14)] == '"Expected":':				
+		id_list.append(data[(i+5):(i+10)])	
+
+print timesArray[0]

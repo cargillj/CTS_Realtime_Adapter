217d0b18fce16a3437031773c6c56145af979cc5
John Cargill <cargillj@onid.orst.edu>
2014-04-19 15:14:27 -0700

    added bus_data array

--- parser.py
+++ parser.py
@@ -19,13 +19,17 @@ timesArray = []
 #text file containing the information in gtfs format 
 gtfsUpdateFile = open("gtfsUpdateFile.txt", "w")
 
-for iterator in range(0, 5):
+for iterator in range(0, 10):
 	response = urllib2.urlopen('http://www.corvallis-bus.appspot.com/arrivals?stops=' + id_list[iterator])
 	apiresponse = response.read()	
 	timesArray.append(apiresponse)
 
-for i in range(0, len(timesArray)): 
-	 if timesArray[i:(i+14)] == '"Expected":':				
-		id_list.append(data[(i+5):(i+10)])	
+bus_data = [[], [], []]
 
-print timesArray[0]
+for i in range(0, len(timesArray)): 
+	bus_data[0].append(id_list[i])
+	bus_data[1].append(timesArray[i][32:37])
+	bus_data[2].append(timesArray[i][81:86])
+
+print timesArray[9]
+#{"10090":[{"Expected":"19 Apr 14 15:52 -0700","Route":"3","Scheduled":"19 Apr 14 15:52 -0700"}

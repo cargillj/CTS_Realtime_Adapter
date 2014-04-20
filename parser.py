606241fd2c3c50339067c8165739db24282232b8
John Cargill <cargillj@onid.orst.edu>
2014-04-19 17:43:48 -0700

    ints are put in the array now

--- parser.py
+++ parser.py
@@ -1,5 +1,13 @@
 import urllib2
 
+# converts strings '12:00' to ints for comparison
+def strTimeToInt(time):
+	hours = int(time[0:2]) * 60
+	mins = int(time[3:5])
+	result = hours + mins
+	return result
+
+
 #this calls the stop IDs for processing and saving into a list
 response = urllib2.urlopen('http://www.corvallis-bus.appspot.com/stops')
 data = response.read()
@@ -18,7 +26,7 @@ timesArray = []
 #text file containing the information in gtfs format 
 gtfsUpdateFile = open("gtfsUpdateFile.txt", "w")
 
-for iterator in range(0, 20):
+for iterator in range(0, len(stop_list)):
 	response = urllib2.urlopen('http://www.corvallis-bus.appspot.com/arrivals?stops=' + stop_list[iterator])
 	apiresponse = response.read()	
 	timesArray.append(apiresponse)
@@ -27,7 +35,7 @@ for iterator in range(0, 20):
 #and estimated times, and routes.
 bus_data = [[], []]
 
-for i in range(0, len(timesArray)):
+for i in range(0, len(stop_list)):
 	bus_data[0].append(stop_list[i])
 
 	stop_times = [[], [], []]
@@ -36,7 +44,7 @@ for i in range(0, len(timesArray)):
 		route = ''
 
 		if(timesArray[i][j:(j+8)] == 'Expected'):
-			stop_times[0].append(timesArray[i][(j+21):(j+26)])
+			stop_times[0].append(strTimeToInt(timesArray[i][(j+21):(j+26)]))
 		elif(timesArray[i][(j):(j+5)] == 'Route'):
 			for k in range((j+8), len(timesArray[i])):
 				if(timesArray[i][k] != '"'):
@@ -45,10 +53,12 @@ for i in range(0, len(timesArray)):
 					break
 			stop_times[1].append(route)
 		elif(timesArray[i][j:(j+9)] == 'Scheduled'):
-			stop_times[2].append(timesArray[i][(j+22):(j+27)])
+			stop_times[2].append(strTimeToInt(timesArray[i][(j+22):(j+27)]))
 
 	bus_data[1].append(stop_times)
 
-print bus_data[0][9]
-print bus_data[1][9]
+for i in range(0, len(stop_list)):
+	print bus_data[0][i]
+	print bus_data[1][i]
+
 #{"10090":[{"Expected":"19 Apr 14 15:52 -0700","Route":"3","Scheduled":"19 Apr 14 15:52 -0700"}
